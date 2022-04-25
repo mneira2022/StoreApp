ae46103fd9be3b5d98473b061b0beeaf6d6ac68d
neira.michelle90@outlook.com <neira.michelle90@outlook.com>
2022-04-24 20:25:41 -0700

    Initial revision.

---
+++ StoreApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StoreApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
